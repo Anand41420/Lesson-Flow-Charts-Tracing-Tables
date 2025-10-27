39a4b7e5ae8c26c4cefb11d39102708892f5b28d
Anand41420 <anand.sidhu27@ycdsbk12.ca>
2025-10-27 17:29:52 -0400

    added variable miles to store conversion result

--- src/ConversionTables.java
+++ src/ConversionTables.java
@@ -7,7 +7,7 @@ public class ConversionTables extends ConsoleProgram {
     @Override
     public void run() {
         for (int km = 10; km <=100; km += 10){
-            
+            double miles = km * 0.6214;
         }
     }
 }
