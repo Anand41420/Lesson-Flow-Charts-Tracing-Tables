3d51e1e727a23e78b2a119fd84bdd7d762f688ec
Anand41420 <anand.sidhu27@ycdsbk12.ca>
2025-10-27 17:22:40 -0400

    Printed counter variable

--- src/Odds.java
+++ src/Odds.java
@@ -7,7 +7,7 @@ public class Odds extends ConsoleProgram {
     @Override
     public void run() {
 		for (int counter = 1; counter <= 15; counter += 2) {
-			
+		    System.out.println(counter);
 		}
     }
 }
