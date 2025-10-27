0199cc572c1f6c6df46036fc19474f6981e75bbe
Anand41420 <anand.sidhu27@ycdsbk12.ca>
2025-10-27 17:30:19 -0400

    printed miles

--- src/ConversionTables.java
+++ src/ConversionTables.java
@@ -8,6 +8,7 @@ public class ConversionTables extends ConsoleProgram {
     public void run() {
         for (int km = 10; km <=100; km += 10){
             double miles = km * 0.6214;
+            System.out.println(miles);
         }
     }
 }
